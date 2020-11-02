9887999c5cdec7efe4f9002dd8fea1eb3fcfdbbd
Pedro <pedroaraujo74@gmail.com>
2020-11-02 12:11:14 +0000

    fixed text

--- src/components/organisms/liked/content.js
+++ src/components/organisms/liked/content.js
@@ -1,6 +1,6 @@
 const content = {
   sectionName: "Liked",
-  title: "Liked what you saw?",
+  title: "Like what you see?",
   text: "<p>We would love to hear from you. Don't hesitate to contact us if you are interested in working or colaborating with us.</p> <p>We will be brief, and respond as soon as posible. Luckily, in a few minutes. 😊</p>",
 };
 
