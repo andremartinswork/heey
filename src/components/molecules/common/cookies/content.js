9d7f86f285497515cf3bf2645173f4efaf41467f
Vasco Silva <vascosilva@Vascos-MacBook-Pro.local>
2020-11-08 20:10:55 +0100

    fix typo in content

--- src/components/molecules/common/cookies/content.js
+++ src/components/molecules/common/cookies/content.js
@@ -1,7 +1,7 @@
 const content = {
   title: "Cookies",
   text:
-    "Our website uses cookies to enhance your user experience, personalize content and show more relevante information about our work. By viewing this website, you agree to our use of cookies.",
+    "Our website uses cookies to enhance your user experience, personalize content and show more relevant information about our work. By viewing this website, you agree to our use of cookies.",
   button: {
     buttonTheme: "theme3",
     type: "button",
