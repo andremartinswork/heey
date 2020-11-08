e54e050a9a7d6d9043ca9c8d6b0846b50262ff5d
andremartinswork <andremartins.work@gmail.com>
2020-11-08 14:42:42 +0000

    Legal

--- src/pages/legal-information.js
+++ src/pages/legal-information.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import {
   BannerLegal,
@@ -9,6 +9,10 @@ import bannerLegalContent from '../components/organisms/bannerLegal/content';
 import legalContent from '../components/organisms/legal/content';
 
 export default function LegalInformation() {
+  useEffect(() => {
+    document.querySelector('body').classList.add('done');
+  }, [])
+
   return (
     <>
       <BannerLegal {...bannerLegalContent} />
